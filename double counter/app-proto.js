f9ac2fce3454f1288fa68018ea697a88994bb8e9
Malik Talha <141961749+malik-talha-github@users.noreply.github.com>
2023-08-11 03:52:48 +0500

    Add files via upload
    Welcome to the Double Counters Visualization project, a powerful tool designed to provide an insightful representation of paired data sets through an intuitive and engaging interface. This GitHub project empowers users to visually compare two sets of quantitative information side by side, enabling deeper analysis and understanding.
    
    Key Features:
    
    Dual Counter Display: The project's core feature is the presentation of dual counters. Users can input two distinct sets of numerical data, and the project renders them as separate counters for easy comparison.
    
    Real-time Synchronization: As users input data into the counters, the visual representation updates in real-time, allowing users to witness changes as they occur and enabling dynamic exploration of data relationships.
    
    Interactive Visualization: The dual counters are complemented by an interactive visualization, such as a bar chart or a line graph, offering a comprehensive view of the data trends and disparities.
    
    Customizable Appearance: Tailor the appearance of the counters and visualization to align with your project's branding or design preferences. Choose from various color schemes, fonts, and graphical styles.
    
    Data Insights: The side-by-side presentation makes it easy to discern patterns, variations, and correlations between the two data sets. This feature is invaluable for decision-making, analysis, and storytelling.
    
    User-Friendly Interface: The project's interface is designed with usability in mind. Users, whether experienced data analysts or newcomers, can effortlessly input data and interpret the results.
    
    Responsive Design: The project is optimized for responsiveness across different devices, ensuring that users can access and interact with the double counters visualization seamlessly.
    
    Whether you're visualizing sales figures, survey responses, or any paired data, the Double Counters Visualization GitHub project offers a robust solution to effectively present and compare information. Join us in shaping the future of data analysis and contribute to this open-source project, enabling others to gain deeper insights through the power of dual counter visualization.

---
+++ double counter/app-proto.js
@@ -0,0 +1,37 @@
+function Counter(element, value){
+    // console.log(element,value);
+    // this.counter = element;
+    // this.value = value;
+    // this.resetBtn = element.querySelector(".reset");
+    // this.decreaseBtn = element.querySelector(".decrease");
+    // this.increaseBtn = element.querySelector(".increase");
+    // this.valueDOM = element.querySelector(".value");
+    // this.valueDOM.textContent =this.value;
+    // //bind this to all function
+    // this.increase = this.increase.bind(this);
+    // this.decrease = this.decrease.bind(this);
+    // this.reset=this.reset.bind(this);
+
+    // this.increaseBtn.addEventListener("click",this.increase);
+    // this.decreaseBtn.addEventListener("click",this.decrease);
+    // this.resetBtn.addEventListener("click",this.reset);
+    
+}
+
+Counter.prototype.increase = function(){
+    // console.log(this);
+   
+}
+Counter.prototype.decrease = function(){
+   // console.log(this);
+    // this.value--;
+    // this.valueDOM.textContent = this.value
+}
+Counter.prototype.reset = function(){
+    //console.log(this);
+   
+}
+
+
+
+
